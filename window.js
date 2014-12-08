c0a57eef04c557745cf57a0b6272638a3f17e0b0
notjesseolsson <notjesseolsson@gmail.com>
2014-12-08 17:00:02 -0500

    cube

---
+++ window.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var scene = new THREE.Scene(),
+    camera = new THREE.PerspectiveCamera(
+      45,
+      innerWidth / innerHeight,
+      .1,
+      1000
+    ),
+    light = new THREE.PointLight(),
+    cube = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshLambertMaterial()
+    ),
+    renderer = new THREE.WebGLRenderer()
+
+  cube.position.set(0, 0, -5)
+  scene.add(camera)
+  scene.add(cube)
+  scene.add(light)
+  renderer.setSize(innerWidth, innerHeight)
+  document.body.appendChild(renderer.domElement)
+  render()
+
+  function render() {
+    requestAnimationFrame(render)
+    renderer.render(scene, camera)
+    beforeRender()
+  }
+
+  function beforeRender() {
+    cube.rotation.z += .01
+    cube.rotation.y += .01
+  }
+
+})
